f2ad7e9b29ab6a62f1243de2d0cd9a318ab910b7
DVKunion <2622100059@qq.com>
2022-12-09 16:58:19 +0800

    feat(veinmind-runner): add remote k8s iac scan

--- veinmind-runner/cmd/iac.go
+++ veinmind-runner/cmd/iac.go
@@ -4,34 +4,127 @@ import (
 	"context"
 	"github.com/chaitin/libveinmind/go/cmd"
 	iacApi "github.com/chaitin/libveinmind/go/iac"
+	"github.com/chaitin/libveinmind/go/kubernetes"
 	"github.com/chaitin/libveinmind/go/plugin"
 	"github.com/chaitin/libveinmind/go/plugin/log"
 	"github.com/chaitin/libveinmind/go/plugin/service"
 	"github.com/chaitin/veinmind-tools/veinmind-runner/pkg/git"
+	"github.com/chaitin/veinmind-tools/veinmind-runner/pkg/plugind"
+	"github.com/google/uuid"
+	"io/fs"
+	"io/ioutil"
 	"os"
 	"path"
+	"path/filepath"
 	"regexp"
+	"strings"
 )
 
-var scanIaCCmd = &cmd.Command{
-	Use:   "scan-iac",
-	Short: "perform iac file scan",
-}
+var (
+	tempDir    = ""
+	scanIaCCmd = &cmd.Command{
+		Use:   "scan-iac",
+		Short: "perform iac file scan",
+	}
+	scanLocalCmd = &cmd.Command{
+		Use:      "local",
+		Short:    "perform local iac file scan",
+		PreRunE:  scanPreRunE,
+		PostRunE: scanPostRunE,
+	}
+	scanGitRepoCmd = &cmd.Command{
+		Use:      "git",
+		Short:    "perform git repo iac file scan",
+		PreRunE:  scanIacPreRunE,
+		RunE:     scanGitRepoIaCFile,
+		PostRunE: scanIacPostRunE,
+	}
+	scanK8sConfigCmd = &cmd.Command{
+		Use:      "remote",
+		Short:    "perform scan iac by k8s config",
+		PreRunE:  scanIacPreRunE,
+		RunE:     scanK8sConfig,
+		PostRunE: scanIacPostRunE,
+	}
+	// replace path
+	scanIacPreRunE = func(c *cmd.Command, args []string) error {
+		// init tempDir
+		dir, err := os.MkdirTemp("", uuid.NewString())
+		if err != nil {
+			return err
+		}
+		tempDir = dir
+		// create resource directory if not exist
+		if _, err := os.Open(resourceDirectoryPath); os.IsNotExist(err) {
+			err = os.Mkdir(resourceDirectoryPath, 0600)
+			if err != nil {
+				return err
+			}
+		}
 
-var scanLocalCmd = &cmd.Command{
-	Use:      "local",
-	Short:    "perform local iac file scan",
-	PreRunE:  scanPreRunE,
-	PostRunE: scanPostRunE,
-}
+		// discover plugins
+		ctx = c.Context()
+		ctx, cancel = context.WithCancel(ctx)
+		ps = []*plugin.Plugin{}
 
-var scanGitRepoCmd = &cmd.Command{
-	Use:      "git",
-	Short:    "perform git repo iac file scan",
-	PreRunE:  scanPreRunE,
-	RunE:     scanGitRepoIaCFile,
-	PostRunE: scanPostRunE,
-}
+		glob, err := c.Flags().GetString("glob")
+		if err == nil && glob != "" {
+			allPlugins, err = plugin.DiscoverPlugins(ctx, ".", plugin.WithGlob(glob))
+		} else {
+			allPlugins, err = plugin.DiscoverPlugins(ctx, ".")
+		}
+		if err != nil {
+			return err
+		}
+
+		serviceManager, err = plugind.NewManager()
+		if err != nil {
+			return err
+		}
+
+		for _, p := range allPlugins {
+			log.Infof("Discovered plugin: %#v\n", p.Name)
+			err = serviceManager.StartWithContext(ctx, p.Name)
+			if err != nil {
+				log.Errorf("%#v can not work: %#v\n", p.Name, err)
+				continue
+			}
+			ps = append(ps, p)
+		}
+
+		// reporter channel listen
+		go runnerReporter.Listen()
+
+		// event channel listen
+		go func() {
+			for {
+				select {
+				case evt := <-reportService.EventChannel:
+					// replace temp path
+					if realID, err := filepath.Rel(tempDir, evt.ID); err == nil {
+						evt.ID = realID
+					}
+					for _, detail := range evt.AlertDetails {
+						if detail.IaCDetail != nil {
+							if realPath, err := filepath.Rel(tempDir, detail.IaCDetail.FileInfo.FilePath); err == nil {
+								detail.IaCDetail.FileInfo.FilePath = realPath
+							}
+						}
+					}
+					runnerReporter.EventChannel <- evt
+				}
+			}
+		}()
+
+		return nil
+	}
+	scanIacPostRunE = func(c *cmd.Command, args []string) error {
+		if tempDir != "" {
+			os.Remove(tempDir)
+		}
+		return scanPostRunE(c, args)
+	}
+)
 
 func scanIaCFile(c *cmd.Command, iac iacApi.IAC) error {
 	log.Infof("scan Iac: %s, filetype: %s", iac.Path, iac.Type)
@@ -83,14 +176,12 @@ func scanGitRepoIaCFile(c *cmd.Command, args []string) error {
 		if isGitUrl {
 			func() {
 				var opt []iacApi.DiscoverOption
-				tempDir := path.Join("/tmp", git.RandStr(12))
 				err = git.Clone(tempDir, arg, key, insecure)
 				if err != nil {
 					log.Errorf("git download failed: %s", err)
 					// nil point fix
 					return
 				}
-				defer os.RemoveAll(tempDir)
 				discovered, err := iacApi.DiscoverIACs(tempDir, opt...)
 				if err != nil {
 					log.Errorf("git discovered failed: %s", err)
@@ -105,9 +196,61 @@ func scanGitRepoIaCFile(c *cmd.Command, args []string) error {
 	return nil
 }
 
+func scanK8sConfig(c *cmd.Command, args []string) error {
+	kubeconfig, _ := c.Flags().GetString("kubeconfig")
+	log.Infof("start load remote k8s cluster config at %s", kubeconfig)
+	scanCtx := c.Context()
+	iacList := make([]iacApi.IAC, 0)
+
+	option := kubernetes.WithKubeConfig(kubeconfig)
+	kubeRoot, err := kubernetes.New(option)
+
+	if err != nil {
+		return err
+	}
+	kubeNamespaces, err := kubeRoot.Resource("namespaces")
+	if err != nil {
+		return err
+	}
+
+	namespaces, err := kubeNamespaces.List(scanCtx)
+	if err != nil {
+		return err
+	}
+
+	log.Infof("download remote k8s cluster config at %s", tempDir)
+	for _, namespace := range namespaces {
+		optionForNamespace := kubernetes.WithNamespace(namespace)
+		if kubeC, errName := kubernetes.New(option, optionForNamespace); errName == nil {
+			if resource, errResource := kubeC.Resource("pods"); errResource == nil {
+				if pods, errPod := resource.List(scanCtx); errPod == nil {
+					for _, pod := range pods {
+						if podsConfig, errConfig := resource.Get(scanCtx, pod); errConfig == nil {
+							tmpConfigFile := path.Join(tempDir, strings.Join([]string{namespace, pod}, ":"))
+							if errWrite := ioutil.WriteFile(tmpConfigFile, podsConfig, fs.ModePerm); errWrite == nil {
+								iacList = append(iacList, iacApi.IAC{
+									Path: tmpConfigFile,
+									Type: iacApi.Kubernetes,
+								})
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	for _, iac := range iacList {
+		_ = scanIaCFile(c, iac)
+	}
+
+	return nil
+}
+
 func init() {
 	scanIaCCmd.AddCommand(cmd.MapIACCommand(scanLocalCmd, scanIaCFile))
 	scanIaCCmd.AddCommand(scanGitRepoCmd)
+	scanIaCCmd.AddCommand(scanK8sConfigCmd)
 
 	scanIaCCmd.PersistentFlags().String("iac-type", "", "dedicate iac type for iac files")
 	scanIaCCmd.PersistentFlags().Int("threads", 5, "threads for scan action")
@@ -115,6 +258,8 @@ func init() {
 	scanIaCCmd.PersistentFlags().StringP("glob", "g", "", "specifies the pattern of plugin file to find")
 
 	scanGitRepoCmd.Flags().String("proxy", "", "proxy to git like: https://xxxxx or socks5://xxxx")
-	scanGitRepoCmd.Flags().String("ssh-pubkey", "", "auth to git if use by ssh proto")
+	scanGitRepoCmd.Flags().String("sshkey", "", "auth to git if use by ssh proto")
 	scanGitRepoCmd.Flags().Bool("insecure-skip", false, "skip tls config")
+
+	scanK8sConfigCmd.Flags().String("kubeconfig", "", "k8s config file")
 }
